824828ae2103628ac62b6caaab06c84d47e7c179
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-10-09 18:34:52 -0400

    chore: note each of the options

--- tests/ember-app/ember-cli-build.js
+++ tests/ember-app/ember-cli-build.js
@@ -5,6 +5,29 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    babel: {
+      // --> has good impact on build size
+      // --> has no impact on build size
+      // useBuiltIns: false, // default, no polyfill
+      // useBuiltIns: 'entry', // our browsertargets already include what would be polyfilled
+      // --> breaks the build
+      // useBuiltIns: 'usage',
+    },
+    'ember-cli-babel': {
+      // --> environment setup
+      throwUnlessParallelizable: true,
+      enableTypeScriptTransform: true,
+      // --> has good impact on build size
+      // --> has no impact on build size
+      disableEmberModulesAPIPolyfill: true,
+      disableEmberDataPackagesPolyfill: true,
+      disableDebugTooling: true,
+      includeExternalHelpers: false,
+      includePolyfill: false,
+      // --> breaks the build
+      // leaves the ESM, but still concatenates everything, which is then invalid ESM
+      // compileModules: false,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
